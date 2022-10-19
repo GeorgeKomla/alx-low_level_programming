e4a6d8bedbe238dff582a197578a403bcce9d223
GeorgeKomla <georgekomlaagbeka@gmail.com>
2022-10-18 21:18:29 -0700

    task 2 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success/correct)
+**/
+
+int main(void)
+
+{
+	int sammy;
+
+	for (sammy = 0; sammy < 10; sammy++)
+	{
+		printf("%d", sammy);
+	}
+	printf("\n");
+	return (0);
+}
