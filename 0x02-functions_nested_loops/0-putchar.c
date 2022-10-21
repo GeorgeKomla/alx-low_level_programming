b9f07b7f78f42bc2cd132aaf1a267c936d102712
GeorgeKomla <georgekomlaagbeka@gmail.com>
2022-10-21 12:48:42 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *Description: Print main.h using predefined functions
+ *Return: 0, return void
+**/
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+beginning of functions & nessted loops
